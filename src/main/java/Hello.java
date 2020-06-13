aafd248ebcf1a1c8c66474d0ed44102dd711abe2
coderCheng97 <66830643+coderCheng97@users.noreply.github.com>
2020-06-13 21:04:04 +0800

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -8,5 +8,7 @@ public class Hello {
         System.out.println("创建分支");
         System.out.println("主干添加");
         System.out.println("分支添加");
+        System.out.println("github在线更新");
+
     }
 }
